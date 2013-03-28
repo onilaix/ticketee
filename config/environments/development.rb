1ce7a9421329b5ea0bf43146adfed4523cfecd9d
onilaix <marco.scialino@gmail.com>
2013-03-28 14:21:57 +0100

    added feature for ensuring a user is signes in

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,4 +27,8 @@ Ticketee::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  # from rais g devise:install output instructions
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,4 +36,7 @@ Ticketee::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+  # from rais g devise:install output instructions
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  
 end
