5e1c0f6c2af0e878a382acc5d74c955933a8f703
onilaix <marco.scialino@gmail.com>
2013-04-03 23:29:39 +0200

    correct sign in missing feature in home page

--- spec/integration/admin/creating_users_spec.rb
+++ spec/integration/admin/creating_users_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+
 feature "Creating Users" do
   let!(:admin) { Factory(:admin_user) }
   before do
@@ -7,13 +8,13 @@ feature "Creating Users" do
     click_link "Admin"
     click_link "Users"
     click_link "New User"
-end
+  end
   scenario 'Creating a new user' do
     fill_in "Email", :with => "newbie@example.com"
     fill_in "Password", :with => "password"
     click_button "Create User"
     page.should have_content("User has been created.")
-end
+  end
   scenario "Leaving email blank results in an error" do
     fill_in "Email", :with => ""
     fill_in "Password", :with => "password"
@@ -21,5 +22,15 @@ end
     page.should have_content("User has not been created.")
     page.should have_content("Email can't be blank")
   end
+
+  scenario "Creating an admin user" do
+    fill_in "Email", :with => "admin@example.com"
+    fill_in "Password", :with => "password"
+    check "Is an admin?"
+    click_button "Create User"
+    page.should have_content("User has been created")
+    within("#users") do
+      page.should have_content("admin@example.com (Admin)")
+    end
+  end
 end
-  
