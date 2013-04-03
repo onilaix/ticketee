af1655604c45d2b8981f0db7dee3ae8b0f7e91d5
onilaix <marco.scialino@gmail.com>
2013-04-03 22:54:56 +0200

    Add the ability to create users through the admin backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Ticketee::Application.routes.draw do
   get "/admin/users/index"
+  namespace :admin do
+    root :to => "base#index"
+    resources :users
+  end
 
   devise_for :users
 
@@ -15,9 +19,9 @@ Ticketee::Application.routes.draw do
   # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
-     resources :projects do
-       resources :tickets
-     end
+  resources :projects do
+    resources :tickets
+  end
 
   # Sample resource route with options:
   #   resources :products do
@@ -56,9 +60,9 @@ Ticketee::Application.routes.draw do
   # just remember to delete public/index.html.
   root :to => 'projects#index'
 
-  # See how all your routes lay out with "rake routes"
+# See how all your routes lay out with "rake routes"
 
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+# This is a legacy wild controller route that's not recommended for RESTful applications.
+# Note: This route will make all actions in every controller accessible via GET requests.
+# match ':controller(/:action(/:id(.:format)))'
 end
