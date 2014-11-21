da93a387f5c5ab9fd7ceade13ab6b305deb5ff99
boss <marco.scialino@gmail.com>
2014-11-21 14:35:14 +0100

    patch to make admin authorization work from the web

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -48,7 +48,7 @@ private
 
 def authorize_admin!
   require_signin!
-  unless current_user.admin?
+  unless current_user.nil? || current_user.admin? 
     flash[:alert] = "You must be an admin to do that."
     redirect_to root_path
   end
