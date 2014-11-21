612445468ee5ea5403d9b14d04be07417c75458f
boss <marco.scialino@gmail.com>
2014-11-21 11:32:16 +0100

    Restrict access to project actions to admins only (page 208).

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,10 +1,23 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe ProjectsController, :type => :controller do
-  it "displays an error for a missing project" do
-    get :show, id: "not-here"
-    expect(response).to redirect_to(projects_path)
-    message = "The project you were looking for could not be found."
-    expect(flash[:alert]).to eql(message)
+describe ProjectsController do
+  let(:user) { FactoryGirl.create(:user) }
+  context "standard users" do
+#    before do
+#     sign_in(user)
+#    end
+    { new: :get,
+      create: :post,
+      edit: :get,
+      update: :put,
+      destroy: :delete }.each do |action, method|
+        it "cannot access the #{action} action" do
+          sign_in(user)
+          send(method, action, :id => FactoryGirl.create(:project))
+          expect(response).to redirect_to(root_path)
+          expect(flash[:alert]).to eql("You must be an admin to do that.")
+        end
+      end 
   end
 end
+
