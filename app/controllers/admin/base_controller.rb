24ce89c284d470a1fbfbfc97a0d62b0c4394b30b
boss <marco.scialino@gmail.com>
2014-11-28 16:03:21 +0100

    Add admin namespaced users controller (page 221)

---
+++ app/controllers/admin/base_controller.rb
@@ -0,0 +1,14 @@
+class Admin::BaseController < ApplicationController
+  before_action :authorize_admin!
+
+private 
+
+def authorize_admin!
+  require_signin!
+  unless current_user.nil? || current_user.admin? 
+    flash[:alert] = "You must be an admin to do that."
+    redirect_to root_path
+  end
+end
+
+end
