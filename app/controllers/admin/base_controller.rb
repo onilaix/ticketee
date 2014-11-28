61a3e0422a2561d78d84be0d57e03bb9019a4e42
boss <marco.scialino@gmail.com>
2014-11-28 17:34:01 +0100

    Add the ability to create users through admin back end (page 229)

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,14 +1,17 @@
 class Admin::BaseController < ApplicationController
   before_action :authorize_admin!
-
-private 
-
-def authorize_admin!
-  require_signin!
-  unless current_user.nil? || current_user.admin? 
-    flash[:alert] = "You must be an admin to do that."
-    redirect_to root_path
+  
+  def index
   end
-end
+
+  private
+
+  def authorize_admin!
+    require_signin!
+    unless current_user.nil? || current_user.admin?
+      flash[:alert] = "You must be an admin to do that."
+      redirect_to root_path
+    end
+  end
 
 end
