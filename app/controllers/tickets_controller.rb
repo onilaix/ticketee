e941c0c7e77de10570f0ca248cf62f6a06da23da
boss <marco.scialino@gmail.com>
2014-12-04 16:11:41 +0100

    Restrict reading tickets to correct project scope (page 275)

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,48 +1,51 @@
 class TicketsController < ApplicationController
   before_action :set_project
   before_action :set_ticket, only: [:show, :edit, :update, :destroy]
-  before_action :require_signin!, except: [:show, :index]
-
+  before_action :require_signin!
   def new
     @ticket = @project.tickets.build
   end
 
-def create
-  @ticket = @project.tickets.build(ticket_params)
-  @ticket.user = current_user
-  if @ticket.save
-    flash[:notice] = "Ticket has been created."
-    redirect_to [@project, @ticket]
-  else
-    flash[:alert] = "Ticket has not been created."
-    render "new"
+  def create
+    @ticket = @project.tickets.build(ticket_params)
+    @ticket.user = current_user
+    if @ticket.save
+      flash[:notice] = "Ticket has been created."
+      redirect_to [@project, @ticket]
+    else
+      flash[:alert] = "Ticket has not been created."
+      render "new"
+    end
   end
-end
 
-def edit
-end
-
-def update
-  if @ticket.update(ticket_params)
-    flash[:notice] = "Ticket has been updated."
-    redirect_to [@project, @ticket]
-  else
-    flash[:alert] = "Ticket has not been updated."
-    render action: "edit"
+  def edit
   end
-end
 
-def destroy
-  @ticket.destroy
-  flash[:notice] = "Ticket has been deleted."
-  redirect_to @project
-end
+  def update
+    if @ticket.update(ticket_params)
+      flash[:notice] = "Ticket has been updated."
+      redirect_to [@project, @ticket]
+    else
+      flash[:alert] = "Ticket has not been updated."
+      render action: "edit"
+    end
+  end
 
-private
+  def destroy
+    @ticket.destroy
+    flash[:notice] = "Ticket has been deleted."
+    redirect_to @project
+  end
+
+  private
 
   def set_project
-    @project = Project.find(params[:project_id])
-  end
+    @project = Project.for(current_user).find(params[:project_id])
+    rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "The project you were looking " +
+    "for could not be found."
+    redirect_to root_path
+    end
 
   def set_ticket
     @ticket = @project.tickets.find(params[:id])
